78dcb6e40089f0c1acc24d5f6e15938655196db8
Naufal Ikhsan <naufal.ikhsan@ui.ac.id>
2019-02-26 14:58:55 +0700

    modul 1

---
+++ jstore/Invoice.java
@@ -0,0 +1,121 @@
+
+/**
+ * Write a description of class Invoice here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Invoice
+{
+<<<<<<< HEAD
+    // instance variables - replace the example below with your own
+    private int x;
+    protected int id;
+    protected int idItem;
+    protected String date;
+    protected int totalPrice;
+
+    /**
+     * Constructor for objects of class Invoice
+     */
+    public Invoice()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+    public int getId(){
+        return 0;
+    }
+    public int getidIdItem(){
+        return 0;
+    }
+    public String getDate(){
+        return "";
+    }
+    public int getTotalPrice(){
+        return 0;
+    }
+    public int setId(int id){
+        return 0;
+    }
+    public int setIdItem(int idItem){
+        return 0;
+    }
+    public String setDate(String date){
+        return "";
+    }
+    public int setTotalPrice(int totalPrice){
+        return 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
+=======
+    protected int id;
+    protected int iditem;
+    protected String date;
+    protected int totalprice;
+
+
+    public Invoice()
+    {
+
+    }
+
+    public int getId()
+    {
+        return 0;
+    }
+    
+    public int getiditem()
+    {
+        return 0;
+    }
+    
+    public String getdate()
+    {
+        return "";
+    }
+    
+    public int gettotalPrice()
+    {
+        return 0;
+    }
+    
+   
+    public int setId(int id)
+    {
+        return 0;
+    }
+    
+    public int setidItem(int idItem)
+    {
+        return 0;
+    }
+    
+    public String setdate(String date)
+    {
+        return "";
+    }
+    
+    public int settotalPrice(int totalPrice)
+    {
+        return 0;
+    }
+    
+    public String setCategory(String category)
+    {
+        return "";
+    }
+}
+>>>>>>> d359b91b709b33ac96a60484eb0e525bb346e6c7
---
+++ jstore/Item.java
@@ -0,0 +1,130 @@
+
+/**
+ * Write a description of class Item here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Item
+{
+<<<<<<< HEAD
+    // instance variables - replace the example below with your own
+    private int x;
+    protected int id;
+    protected String name;
+    protected int stock;
+    protected int price;
+    protected String category;
+
+    /**
+     * Constructor for objects of class Item
+     */
+    public Item()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+    public int getId(){
+        return 0;
+    }
+    public String getName(){
+        return "";
+    }
+    public int getStock(){
+        return 0;
+    }
+    public int getPrice(){
+        return 0;
+    }
+    public String getCategory(){
+        return "";
+    }
+    public int setId(int id){
+        return 0;
+    }
+    public String setName(String name){
+        return "";
+    }
+    public int setStock(int stock){
+        return 0;
+    }
+    public int setPrice(int price){
+        return 0;
+    }
+    public String setCategory(String category){
+        return "";
+    }
+    
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
+
+=======
+protected     int id;
+protected     string name;
+protected     int stock;
+protected     int price;
+protected     string category;    
+
+public item()
+{return 0
+}
+
+public int getId()
+{ return 0;
+}
+
+public string getName()
+{ return "";
+}
+
+
+public int getStock()
+{ return 0;
+}
+
+public int getPrice()
+{ return 0;
+}
+
+public string getCategory()
+{ return "";
+}
+
+
+public id int setId()
+{ return 0;
+}
+
+public setName(name:string)
+{ return "";
+}
+
+
+public setStock(stock:int)
+{ return 0;
+}
+
+public setPrice(price:int)
+{ return 0;
+}
+
+public SetCategory(category:string)
+{ return "";
+}
+
+
+
+}
+}
+>>>>>>> d359b91b709b33ac96a60484eb0e525bb346e6c7
